6811ee52c65e77b3a91ab1d4d6c2ab2c7aee111c
Sid <siddharth_shah@live.com>
2019-03-26 14:26:33 +0800

    changed named of analyzer to acquire

--- acquire/tests/test_csv_parser.py
+++ acquire/tests/test_csv_parser.py
@@ -9,6 +9,5 @@ class TestCsvParser(unittest.TestCase) :
     def test_if_file_not_present_throws_error(self) : 
         self.assertRaises(FileNotFoundError, lambda: csv_parser.parse("invalid_file_path"))
 
-
 if __name__ == '__main__':
     unittest.main()
