03de171c447f60bc039c8df49f55e38a33f2ae53
Sid <siddharth_shah@live.com>
2019-03-27 14:29:06 +0800

    improving setup_venv_locally.sh

--- bin/setup_venv_locally.sh
+++ bin/setup_venv_locally.sh
@@ -2,12 +2,12 @@
 
 project_path=$(dirname $0)/..
 
-cd $project_path
-export PYTHONPATH=$project_path
+cd ${project_path}
+export PYTHONPATH=${project_path}
 
 echo "$header: Creating virtual environment."
-python3 -m venv $project_path/.venv
-source $project_path/.venv/bin/activate
+python3 -m venv ${project_path}/.venv
+source ${project_path}/.venv/bin/activate
 
 curl https://bootstrap.pypa.io/get-pip.py | python
 pip install -r requirements.txt
