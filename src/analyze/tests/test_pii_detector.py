c84e0f1383ceaa83eeadb73633cd847dcfd10df8
Sid <siddharth_shah@live.com>
2019-03-28 11:36:06 +0800

    Changed test case for PIIDetector to include change in EmailDetector name to all caps for consistency

--- src/analyze/tests/test_pii_detector.py
+++ src/analyze/tests/test_pii_detector.py
@@ -10,11 +10,11 @@ class TestPIIDetector(TestCase):
 
     def test_should_detect_email_in_text(self):
         results = PIIDetector.analyze("A typical email id would look something like test@sample.com")
-        self.assertEqual("The text test@sample.com at position(45,60) was identified as Email", results[0])
+        self.assertEqual("The text test@sample.com at position(45,60) was identified as EMAIL", results[0])
 
     def test_should_detect_nric_and_email_in_text(self):
         results = PIIDetector.analyze("""First President of Singapore NRIC was S0000001I.
                                          A typical email id would look something like test@sample.com""")
 
         self.assertEqual("The text S0000001I at position(38,47) was identified as NRIC", results[0])
-        self.assertEqual("The text test@sample.com at position(135,150) was identified as Email", results[1])
+        self.assertEqual("The text test@sample.com at position(135,150) was identified as EMAIL", results[1])
