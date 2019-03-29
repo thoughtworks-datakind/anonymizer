9018e59a54ccc2d0c915c426a53c1b4799a9e531
Sid <siddharth_shah@live.com>
2019-03-29 13:36:57 +0800

    Renaming variable names

--- src/analyze/tests/test_national_id_detector.py
+++ src/analyze/tests/test_national_id_detector.py
@@ -5,14 +5,14 @@ from analyze.national_id_detector import NationalIdDetector
 class TestNationalIdDetector(TestCase):
 
     def setUp(self):
-        self.gov_id_detector = NationalIdDetector()
+        self.national_id_detector = NationalIdDetector()
 
     def test_default_property_values_are_correct(self):
-        self.assertEqual("NRIC", self.gov_id_detector.name)
-        self.assertEqual("[STFG]\\d{7}[A-Z]", self.gov_id_detector.pattern)
+        self.assertEqual("NRIC", self.national_id_detector.name)
+        self.assertEqual("[STFG]\\d{7}[A-Z]", self.national_id_detector.pattern)
 
     def test_additional_checks_should_return_true_when_given_valid_input(self):
-        self.assertTrue(self.gov_id_detector.validate("S0000001I"))
+        self.assertTrue(self.national_id_detector.validate("S0000001I"))
 
     def test_additional_checks_should_return_true_when_given_invalid_input(self):
-        self.assertFalse(self.gov_id_detector.validate("S0000001K"))
+        self.assertFalse(self.national_id_detector.validate("S0000001K"))
