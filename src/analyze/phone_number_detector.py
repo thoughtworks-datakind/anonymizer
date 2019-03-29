0394c30d8d763aa89355716e0bdbd8d1014b6ef3
Sid <siddharth_shah@live.com>
2019-03-29 12:34:39 +0800

    removed comments

--- src/analyze/phone_number_detector.py
+++ src/analyze/phone_number_detector.py
@@ -6,12 +6,9 @@ class PhoneNumberDetector(BaseDetector):
 
     def __init__(self):
         self.name = "PHONE NUMBER"
-        #self.assertEqual('[+]*[(]?[0-9]{1,4}[)]?[-\s\./0-9]*', phone_number_detector.pattern)
         self.pattern = RegEx()\
             .one_of("+").zero_or_more_occurrences()\
             .one_of("(").zero_or_one_occurrences()\
             .range(0, 9).range_occurrences(1, 4)\
             .one_of(")").zero_or_one_occurrences()\
             .one_of("-\s\./0-9").zero_or_more_occurrences().build()
-
-
