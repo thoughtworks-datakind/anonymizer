cb5452caa0d7dbc771c39653bcf30c838a892397
Sid <siddharth_shah@live.com>
2019-03-29 11:40:20 +0800

    added test case for analyzer result str representation

--- src/analyze/tests/test_analyzer_result.py
+++ src/analyze/tests/test_analyzer_result.py
@@ -14,3 +14,11 @@ class TestAnalyzerResult(TestCase):
         self.assertNotEqual(AnalyzerResult("text", "type", 0, 10), AnalyzerResult("text", "different_type", 0, 10))
         self.assertNotEqual(AnalyzerResult("text", "type", 0, 10), AnalyzerResult("text", "type", 1, 10))
         self.assertNotEqual(AnalyzerResult("text", "type", 0, 10), AnalyzerResult("text", "type", 0, 11))
+
+    def test_repr(self):
+        expected = "Text sample_data at position (0,10) was identified as type"
+        self.assertEqual(AnalyzerResult("sample_data", "type", 0, 10).__repr__(), expected)
+
+    def test_str(self):
+        expected = "Text sample_data at position (0,10) was identified as type"
+        self.assertEqual(str(AnalyzerResult("sample_data", "type", 0, 10)), expected)
