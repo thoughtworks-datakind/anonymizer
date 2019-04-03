12e02a3855d51d48291c524da96690fadd3400af
Sid <siddharth_shah@live.com>
2019-04-03 11:44:58 +0800

    [ANONYMIZER-7] : Changed the regex part by splitting it into multiple strings

--- src/analyze/phone_number_detector.py
+++ src/analyze/phone_number_detector.py
@@ -1,18 +1,28 @@
 import re
 
 from src.analyze.base_detector import BaseDetector
+from src.analyze.regex import RegEx
 
 
 class PhoneNumberDetector(BaseDetector):
 
     def __init__(self):
         self.name = "PHONE_NUMBER"
-        self.pattern = '(\\+65?\\s?[689]\\d{7})|' \
-                       '(\\+65?\\s?[689]\\d{3} \\d{4})|' \
-                       '([689]\\d{7})|' \
-                       '([689]\\d{3} \\d{4})|' \
-                       '([(]65[)]\\s?[689]\\d{7})|' \
-                       '([(]65[)]\\s?[689]\\d{3} \\d{4})'
+        regex_pipe = RegEx().pipe().build()
+
+        regex_with_country_code_and_no_space = '(\\+65?\\s?[689]\\d{7})'
+        regex_with_country_code_and_single_space = '(\\+65?\\s?[689]\\d{3} \\d{4})'
+        regex_no_country_code_and_no_space = '([689]\\d{7})'
+        regex_no_country_code_and_single_space = '([689]\\d{3} \\d{4})'
+        regex_with_country_code_in_brackets_and_no_space = '([(]65[)]\\s?[689]\\d{7})'
+        regex_with_country_code_in_brackets_and_single_space = '([(]65[)]\\s?[689]\\d{3} \\d{4})'
+
+        self.pattern = regex_with_country_code_and_no_space + regex_pipe + \
+            regex_with_country_code_and_single_space + regex_pipe + \
+            regex_no_country_code_and_no_space + regex_pipe + \
+            regex_no_country_code_and_single_space + regex_pipe + \
+            regex_with_country_code_in_brackets_and_no_space + regex_pipe + \
+            regex_with_country_code_in_brackets_and_single_space
 
     def get_name(self):
         return self.name
