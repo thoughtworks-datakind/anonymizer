41ce735b172d6e318c9a5371b3db8a431e8f6b16
Sid <siddharth_shah@live.com>
2019-04-03 11:02:27 +0800

    [ANONYMIZER-7] : Added multiple patterns and test cases to phone)number_detector to make it more robust

--- src/analyze/phone_number_detector.py
+++ src/analyze/phone_number_detector.py
@@ -1,19 +1,18 @@
 import re
 
 from src.analyze.base_detector import BaseDetector
-from src.analyze.regex import RegEx
 
 
 class PhoneNumberDetector(BaseDetector):
 
     def __init__(self):
-        self.name = "PHONE NUMBER"
-        self.pattern = RegEx()\
-            .one_of("+").zero_or_more_occurrences()\
-            .one_of("(").zero_or_one_occurrences()\
-            .range(0, 9).range_occurrences(1, 4)\
-            .one_of(")").zero_or_one_occurrences()\
-            .one_of("-\s\./0-9").zero_or_more_occurrences().build()
+        self.name = "PHONE_NUMBER"
+        self.pattern = '(\\+65?\\s?[689]\\d{7})|' \
+                       '(\\+65?\\s?[689]\\d{3} \\d{4})|' \
+                       '([689]\\d{7})|' \
+                       '([689]\\d{3} \\d{4})|' \
+                       '([(]65[)]\\s?[689]\\d{7})|' \
+                       '([(]65[)]\\s?[689]\\d{3} \\d{4})'
 
     def get_name(self):
         return self.name
--- src/analyze/tests/test_phone_number_detector.py
+++ src/analyze/tests/test_phone_number_detector.py
@@ -1,10 +1,53 @@
 from unittest import TestCase
 from src.analyze.phone_number_detector import PhoneNumberDetector
+from src.analyze.analyzer_result import AnalyzerResult
 
 
 class TestPhoneNumberDetector(TestCase):
 
+    def setUp(self):
+        self.phone_number_detector = PhoneNumberDetector()
+
     def test_default_property_values_are_correct(self):
-        phone_number_detector = PhoneNumberDetector()
-        self.assertEqual("PHONE NUMBER", phone_number_detector.name)
-        self.assertEqual('[+]*[(]?[0-9]{1,4}[)]?[-\\s\\./0-9]*', phone_number_detector.pattern)
+        self.assertEqual("PHONE_NUMBER", self.phone_number_detector.name)
+        self.assertEqual('(\\+65?\\s?[689]\\d{7})|'
+                         '(\\+65?\\s?[689]\\d{3} \\d{4})|'
+                         '([689]\\d{7})|'
+                         '([689]\\d{3} \\d{4})|'
+                         '([(]65[)]\\s?[689]\\d{7})|'
+                         '([(]65[)]\\s?[689]\\d{3} \\d{4})',
+                         self.phone_number_detector.pattern)
+
+    def test_invalid_phone_number_does_not_get_detected(self):
+        self.assertEqual(len(self.phone_number_detector.execute("S0000001I")), 0)
+
+    def __assert_single_result(self, text_to_be_tested, start, end):
+        actual = self.phone_number_detector.execute(text_to_be_tested)
+        expected = AnalyzerResult(text_to_be_tested, "PHONE_NUMBER", start, end)
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(expected, actual[0])
+
+    def test_valid_phone_number_gets_detected_correctly(self):
+        self.__assert_single_result("+65 65781234", 0, 12)
+        self.__assert_single_result("+65 85781234", 0, 12)
+        self.__assert_single_result("+65 95781234", 0, 12)
+
+        self.__assert_single_result("+65 6578 1234", 0, 13)
+        self.__assert_single_result("+65 8578 1234", 0, 13)
+        self.__assert_single_result("+65 9578 1234", 0, 13)
+
+        self.__assert_single_result("65781234", 0, 8)
+        self.__assert_single_result("85781234", 0, 8)
+        self.__assert_single_result("95781234", 0, 8)
+
+        self.__assert_single_result("6578 1234", 0, 9)
+        self.__assert_single_result("8578 1234", 0, 9)
+        self.__assert_single_result("9578 1234", 0, 9)
+
+        self.__assert_single_result("(65) 65781234", 0, 13)
+        self.__assert_single_result("(65) 85781234", 0, 13)
+        self.__assert_single_result("(65) 95781234", 0, 13)
+
+        self.__assert_single_result("(65) 6578 1234", 0, 14)
+        self.__assert_single_result("(65) 8578 1234", 0, 14)
+        self.__assert_single_result("(65) 9578 1234", 0, 14)
