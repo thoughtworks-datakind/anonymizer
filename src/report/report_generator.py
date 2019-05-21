40d5269f7797dc18a0e92b61d5139a6e5f7dfc93
skrishnan@thoughtworks.com <skrishnan@thoughtworks.com>
2019-05-21 15:53:35 +0800

    [ANONYMIZER-8] Treating each cell as a list of analyzer results; concatenating the column report series to report df

--- src/report/report_generator.py
+++ src/report/report_generator.py
@@ -16,20 +16,32 @@ class ReportGenerator():
     def __generate_high_level_report(self, results_df):
         return results_df.columns.values
 
+    def __collate_all_detectors_per_cell(self, analyzer_result):
+        return [result.detector() for result in analyzer_result[1]]
+
+    #TODO : filter out the NAs before passing through this
     def calculate_detector_counts_for_each_column(self, column_series):
         detector_map = {}
-        for analyzer_result in column_series.iteritems():
-            detector_type = analyzer_result[1].detector()
-            if detector_type not in detector_map:
-                detector_map[detector_type] = 1
-            detector_map[detector_type] += 1
+        for analyzer_results in column_series.iteritems():
+            if not analyzer_results:
+                continue
+            detector_types = self.__collate_all_detectors_per_cell(analyzer_results)
+            for detector_type in detector_types:
+                if detector_type not in detector_map:
+                    detector_map[detector_type] = 0
+                detector_map[detector_type] += 1
         return detector_map
 
     def __generate_medium_level_report(self, results_df):
         report_df = pd.DataFrame()
-        for column in list(results_df):
+        columns = list(results_df)
+        column_reports = []
+        for column in columns:
             detector_counts_for_each_column = self.calculate_detector_counts_for_each_column(results_df[column])
-            report_df[column] = pd.Series(detector_counts_for_each_column)
+            column_report = pd.Series(detector_counts_for_each_column, name=column, index=detector_counts_for_each_column.keys())
+            if not column_report.empty:
+                column_reports.append(column_report)
+        report_df = pd.concat(column_reports, axis=1, keys=[series.name for series in column_reports], sort=True)
         return report_df
 
     def __generate_report_content(self, results_df, report_level):
--- src/report/tests/test_report_generator.py
+++ src/report/tests/test_report_generator.py
@@ -17,20 +17,19 @@ class TestReportGenerator(TestCase):
         self.assertTrue(self.report_generator.generate(result_data_frame).empty)
 
     def test_high_level_reporting_returns_columns_with_PII_values_when_given_a_results_data_frame(self):
-        result_data_frame = pd.DataFrame({"summary" : [AnalyzerResult("S0000001I", "NRIC", 38, 47), AnalyzerResult("test@sample.com", "EMAIL", 45, 60)],
-                                         "phone number": [AnalyzerResult("+65 62345678", "PHONE_NUMBER", 35, 47), AnalyzerResult("+65 62345678", "PHONE_NUMBER", 35, 47)]})
+        result_data_frame = pd.DataFrame({"summary" : [[AnalyzerResult("S0000001I", "NRIC", 38, 47)], [AnalyzerResult("test@sample.com", "EMAIL", 45, 60)]],
+                                         "phone number": [[AnalyzerResult("+65 62345678", "PHONE_NUMBER", 35, 47)], [AnalyzerResult("+65 62345678", "PHONE_NUMBER", 35, 47)]]})
         self.assertCountEqual(self.report_generator.generate(result_data_frame), ["summary", "phone number"])
 
     def test_medium_level_reporting_returns_data_frame_with_detectors_and_column_details(self):
-        result_data_frame = pd.DataFrame({"summary" : [AnalyzerResult("S0000001I", "NRIC", 38, 47), AnalyzerResult("test@sample.com", "EMAIL", 45, 60)],
-                                          "phone number": [AnalyzerResult("+65 62345678", "PHONE_NUMBER", 35, 47), AnalyzerResult("+65 62345678", "PHONE_NUMBER", 35, 47)]})
-
+        result_data_frame = pd.DataFrame({"summary" : [[AnalyzerResult("S0000001I", "NRIC", 38, 47)], [AnalyzerResult("test@sample.com", "EMAIL", 45, 60)]],
+                                          "phone number": [[AnalyzerResult("+65 62345678", "PHONE_NUMBER", 35, 47)], [AnalyzerResult("+65 62345678", "PHONE_NUMBER", 35, 47)]]})
         expected_data_frame = pd.DataFrame({"summary" : pd.Series({"NRIC" : 1, "EMAIL" : 1}),
                                            "phone number" : pd.Series({"PHONE_NUMBER" : 2})})
         self.assertCountEqual(list(expected_data_frame), self.report_generator.generate(result_data_frame, report_level=ReportLevel.MEDIUM))
 
     def test_calculate_detector_counts_skims_through_a_column_and_counts_detector_results_in_each_of_them(self):
-        result_column_values = pd.Series(np.array([AnalyzerResult("S0000001I", "NRIC", 38, 47), AnalyzerResult("test@sample.com", "EMAIL", 45, 60), AnalyzerResult("test@sample.com", "EMAIL", 45, 60)]))
+        result_column_values = pd.Series([[AnalyzerResult("S0000001I", "NRIC", 38, 47)], [AnalyzerResult("test@sample.com", "EMAIL", 45, 60)], [AnalyzerResult("test@sample.com", "EMAIL", 45, 60)]])
         actual_result = self.report_generator.calculate_detector_counts_for_each_column(result_column_values)
         expected_result = {"NRIC" : 1, "EMAIL" : 2}
         self.assertCountEqual(expected_result, actual_result)
