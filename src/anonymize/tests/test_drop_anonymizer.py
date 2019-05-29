04c1d81f730fffea8c3ab2b853366100483d4910
siddarthshahtw <siddarth.shah@thoughtworks.com>
2019-05-29 15:26:42 +0800

    [ANONYMIZER-11] : Adding more test cases

--- src/anonymize/tests/test_drop_anonymizer.py
+++ src/anonymize/tests/test_drop_anonymizer.py
@@ -5,9 +5,16 @@ from src.analyze.utils.analyzer_result import AnalyzerResult
 
 class TestDropAnonymizer(TestCase):
 
-    def test_execute_calls_match_and_validate(self):
+    def test_redact_for_single_analyzer_result(self):
         text = "text containing pii"
-        analyzer_results = [AnalyzerResult("pii", "ANY_PII_DETECTOR", 27, 29)]
+        analyzer_results = [AnalyzerResult("pii", "PII_DETECTOR", 16, 18)]
         result = DropAnonymizer.redact(text, analyzer_results)
         self.assertEqual(result, "text containing ")
+
+    def test_redact_for_multiple_analyzer_results(self):
+        text = "text containing pii1 and pii2"
+        analyzer_results = [AnalyzerResult("pii1", "PII_DETECTOR", 16, 19),
+                            AnalyzerResult("pii2", "PII_DETECTOR", 25, 28)]
+        result = DropAnonymizer.redact(text, analyzer_results)
+        self.assertEqual(result, "text containing  and ")
 
